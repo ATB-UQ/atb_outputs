6a19aabfb366d81ea0139259df94253edc8596af
Martin Stroet <martin.stroet@gmail.com>
2023-10-25 13:42:19 +1000

    Refactor project

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-#from distutils.core import setup
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='atb_outputs',
-    version='1.0',
-    author='Bertrand Caron',
-    author_email='b.caron@uq.edu.au',
-    url='https://atb.uq.edu.au',
-    packages=find_packages(''),
-    install_requires=['pyyaml', 'numpy'],
-    description='Automated Topology Builder (ATB) molecule data object and output modules (PDB, YML, PICKLE, LGF, GRAPH) ',
-    keywords='ATB PDB graph',
-    python_requires='>=3.5',
-)
+setup()
